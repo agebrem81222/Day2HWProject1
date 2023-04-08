a02104819be268f3df8c0b8fa50f27db75fe2355
alema <alema@10.0.0.114>
2023-04-08 14:11:00 -0400

    adding implemented

--- src/day2git/Day2HW1.java
+++ src/day2git/Day2HW1.java
@@ -6,5 +6,10 @@ public class Day2HW1 {
 		
 		System.out.println("day 2 HW1");
 	}
-
+public void divid(int a, int b) {
+	System.out.println(a/b);
+}
+public void add(int a, int b) {
+	System.out.println(a+b);
+}
 }
